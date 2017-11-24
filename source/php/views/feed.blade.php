04ef418592c9f8c21d94595f4e804bce4ffda533
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-11-24 19:20:46 +0100

    Image size on items.

--- source/php/views/feed.blade.php
+++ source/php/views/feed.blade.php
@@ -7,7 +7,7 @@
             <li class="social-item">
                  <div class="material">
                      <div class="social-image">
-                         <a href="{{ $item['network_source'] }}" class="ratio-1-1" style="display: block; background-size: cover; background-image: url('{{ $item['image_large'] }}');"></a>
+                         <a href="{{ $item['network_source'] }}" class="ratio-16-9" style="display: block; background-size: cover; background-image: url('{{ $item['image_large'] }}');"></a>
                      </div>
                      <div class="social-user">
 
